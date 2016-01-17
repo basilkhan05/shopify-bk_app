ba3efc530dde5f8c6a1379362d4c498a7894356a
basilkhan05 <basilkhan.dev@gmail.com>
2016-01-17 02:04:53 -0500

    generated shopify app

---
+++ config/initializers/shopify_app.rb
@@ -0,0 +1,6 @@
+ShopifyApp.configure do |config|
+  config.api_key = "7d7a705b0032d726c3d34e07d5ac68a3"
+  config.secret = "44fddb5fc329352caa1122e253954814"
+  config.scope = "read_orders, read_products"
+  config.embedded_app = true
+end
